fa9284c9db8fe4c10d3b2f8b5bd7ad1e04602e3a
flatedex <flatedex@mail.ru>
2023-03-21 10:56:17 +0300

    Running .bat file through C# program

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿
+﻿using System.Diagnostics;
+
 namespace BatchFileTest{
     public class Program{
         public static void Main(){
-            
+            string batFileDir = String.Format(@"C:\C# Projects\ASP.NET-MVC\WebArchiver\ExternalPrograms\ArchiverMVC.bat");
+			Process.Start(batFileDir);
         }
     }
 }
